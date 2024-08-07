f1f9eb2203b7b67686ec7fea0acf26d417cb7581
David J. Straley <djstraley@sandyhook.com>
2024-08-07 15:16:01 -0500

    Add project files.

---
+++ MVCApp/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+namespace MVCApp.Data
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<MVCAppContext>
+    {
+        private IConfiguration _configuration { get; }
+        // Create Constructor
+
+        public ApplicationDbContextFactory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public MVCAppContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MVCAppContext>();
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+            //optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=EcommerceDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new MVCAppContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ MVCApp/Data/MVCAppContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCApp.Models;
+using System.Configuration;
+
+namespace MVCApp.Data
+{
+    public class MVCAppContext : DbContext
+    {
+        public MVCAppContext(DbContextOptions<MVCAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contact { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Contact>().ToTable("Contact");
+        //}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=MVCApp;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+
+    }   
+}
